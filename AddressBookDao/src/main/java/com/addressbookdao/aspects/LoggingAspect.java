afb8da460683a68f5515f812c73a723222b5b60a
Joey73 <schmitz873@gmail.com>
2016-09-10 17:10:11 +0200

    Unnecessary Import deleted

--- AddressBookDao/src/main/java/com/addressbookdao/aspects/LoggingAspect.java
+++ AddressBookDao/src/main/java/com/addressbookdao/aspects/LoggingAspect.java
@@ -1,7 +1,5 @@
 package com.addressbookdao.aspects;
 
-import java.util.Arrays;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
